4c95ae31582d998f5b90cdc3218fcce20f25cfd4
chenjunyu19 <1335546814@qq.com>
2017-08-13 20:28:19 +0800

    添加启动器下载

--- script.js
+++ script.js
@@ -23,34 +23,51 @@ function getVersions() {
 }
 
 function genGameDownloadLink(version) {
-  var title = document.getElementById('game-card__title');
-  var output = document.getElementById('game-card__output');
-  title.className = 'mdl-card__title mdl-color--blue mdl-color-text--white';
-  output.innerHTML = '<h1>:|</h1><p>正在生成。</p>';
+  var supporting = document.querySelector('#game-card .mdl-card__supporting-text');
+  var actions = document.querySelector('#game-card .mdl-card__actions');
+  supporting.innerHTML = '<h1>:|</h1><h3>正在生成。</h3>';
   getJson('https://launchermeta.mojang.com/mc/game/version_manifest.json', function (json) {
     for (var i in json.versions) {
       if (json.versions.hasOwnProperty(i) && json.versions[i].id === version) {
         var versionUrl = json.versions[i].url;
         getJson(versionUrl, function (versionJson) {
-          var string = '<h1>:)</h1><p>生成完毕。</p><p>';
-          string += '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="' + versionUrl + '">JSON</a>';
-          string += '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="' + versionJson.downloads.client.url + '">客户端</a>';
+          var infoHTML = '<h1>:)</h1><h3>生成完毕。</h3>';
+          infoHTML += '客户端 SHA-1：<code>' + versionJson.downloads.client.sha1 + '</code>';
           if (versionJson.downloads.hasOwnProperty('server')) {
-            string += '<a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect" href="' + versionJson.downloads.server.url + '">服务端</a>';
+            infoHTML += '<br>服务端 SHA-1：<code>' + versionJson.downloads.server.sha1 + '</code>';
           }
-          string += '</p><p>';
-          string += '客户端 SHA-1：<code>' + versionJson.downloads.client.sha1 + '</code>';
+          var linksHTML = '<a class="mdl-button mdl-button--colored" href="' + versionUrl + '">JSON</a>';
+          linksHTML += '<a class="mdl-button mdl-button--colored" href="' + versionJson.downloads.client.url + '">客户端</a>';
           if (versionJson.downloads.hasOwnProperty('server')) {
-            string += '<br>服务端 SHA-1：<code>' + versionJson.downloads.server.sha1 + '</code>';
+            linksHTML += '<a class="mdl-button mdl-button--colored" href="' + versionJson.downloads.server.url + '">服务端</a>';
           }
-          string += '</p>';
-          output.innerHTML = string;
-          title.className = 'mdl-card__title mdl-color--green mdl-color-text--white';
+          supporting.innerHTML = infoHTML;
+          actions.innerHTML = linksHTML;
         });
         return;
       }
     }
-    title.className = 'mdl-card__title mdl-color--red mdl-color-text--white';
-    output.innerHTML = '<h1>:(</h1><p>生成失败。找不到所选版本，请确认所选版本是否存在或检查网络。</p>';
+    supporting.innerHTML = '<h1>:(</h1><h3>生成失败。找不到所选版本，请确认所选版本是否存在或检查网络。</h3>';
   });
+}
+
+function genLauncherDownloadLink() {
+  var supporting = document.querySelector('#launcher-card .mdl-card__supporting-text');
+  var actions = document.querySelector('#launcher-card .mdl-card__actions');
+  supporting.innerHTML = '<h1>:|</h1><h3>正在生成。</h3>';
+  getJson('https://launchermeta.mojang.com/mc/launcher.json', function (json) {
+    var infoHTML = '<h1>:)</h1><h3>生成完毕。</h3>';
+    infoHTML += '<h4>Java</h4>SHA-1：<code>' + json.java.sha1 + '</code><h5>LZMA</h5>SHA-1：<code>' + json.java.lzma.sha1 + '</code>';
+    infoHTML += '<h4>Linux</h4>Download hash：<code>' + json.linux.downloadhash + '</code><h5>Versions</h5><h6>Launcher</h6>Commit：<code>' + json.linux.versions.launcher.commit + '</code><br>Name：<code>' + json.linux.versions.launcher.name + '</code>';
+    infoHTML += '<h4>OS X</h4>App hash：<code>' + json.osx.apphash + '</code><br>Download hash：<code>' + json.osx.downloadhash + '</code><h5>64 Bits</h5><h6>JDK</h6>SHA-1：<code>' + json.osx[64].jdk.sha1 + '</code><br>Version：<code>' + json.osx[64].jdk.version + '</code><h6>JRE</h6>SHA-1：<code>' + json.osx[64].jre.sha1 + '</code><br>Version：<code>' + json.osx[64].jre.version + '</code><h5>Versions</h5><h6>Launcher</h6>Commit：<code>' + json.osx.versions.launcher.commit + '</code><br>Name：<code>' + json.osx.versions.launcher.name + '</code>';
+    infoHTML += '<h4>Windows</h4>App hash：<code>' + json.windows.apphash + '</code><br>Download hash：<code>' + json.windows.downloadhash + '</code><h5>32 Bits</h5><h6>JDK</h6>SHA-1：<code>' + json.windows[32].jdk.sha1 + '</code><br>Version：<code>' + json.windows[32].jdk.version + '</code><h6>JRE</h6>SHA-1：<code>' + json.windows[32].jre.sha1 + '</code><br>Version：<code>' + json.windows[32].jre.version + '</code><h5>64 Bits</h5><h6>JDK</h6>SHA-1：<code>' + json.windows[64].jdk.sha1 + '</code><br>Version：<code>' + json.windows[64].jdk.version + '</code><h6>JRE</h6>SHA-1：<code>' + json.windows[64].jre.sha1 + '</code><br>Version：<code>' + json.windows[64].jre.version + '</code><h5>Versions</h5><h6>Launcher</h6>Commit：<code>' + json.windows.versions.launcher.commit + '</code><br>Name：<code>' + json.windows.versions.launcher.name + '</code>';
+    var linksHTML = '<a class="mdl-button mdl-button--colored" href="' + json.java.lzma.url + '">Java - LZMA</a>';
+    linksHTML += '<br><a class="mdl-button mdl-button--colored" href="' + json.linux.applink + '">Linux - App</a>';
+    linksHTML += '<br><a class="mdl-button mdl-button--colored" href="' + json.osx.applink + '">OS X - App</a><a class="mdl-button mdl-button--colored" href="' + json.osx[64].jdk.url + '">OS X - 64 Bits - JDK</a><a class="mdl-button mdl-button--colored" href="' + json.osx[64].jre.url + '">OS X - 64 Bits - JRE</a>';
+    linksHTML += '<br><a class="mdl-button mdl-button--colored" href="' + json.windows.applink + '">Windows - App</a><a class="mdl-button mdl-button--colored" href="' + json.windows[32].jdk.url + '">Windows - 32 Bits - JDK</a><a class="mdl-button mdl-button--colored" href="' + json.windows[32].jre.url + '">Windows - 32 Bits - JRE</a><a class="mdl-button mdl-button--colored" href="' + json.windows[64].jdk.url + '">Windows - 64 Bits - JDK</a><a class="mdl-button mdl-button--colored" href="' + json.windows[64].jre.url + '">Windows - 64 Bits - JRE</a>';
+    supporting.innerHTML = infoHTML;
+    actions.innerHTML = linksHTML;
+    return;
+  });
+  supporting.innerHTML = '<h1>:(</h1><h3>生成失败。请检查网络。</h3>';
 }
